2676c91a1bba3f2d06b84df26cfd9bbeee675c13
henriquegmarquess <106783182+henriquegmarquess@users.noreply.github.com>
2022-06-14 23:15:56 -0300

    Add carrossel.
    Add files via upload
    
    Delete teste1.jpg
    
    Delete teste2.jpg
    
    Delete teste3.jpg
    
    Update index.html
    
    Obs: Removi o "onclick="mover_carrossel()"" do primeiro button, pois é uma função que ainda não existe nesta versão do código
    
    .

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-script.js
+let posicao = 0;
+const slides = document.getElementsByClassName('imagens');
+const totalSlides = slides.length;
+
+document.
+  getElementById('proximo')
+  .addEventListener("click", function() {
+    proximo_slide();
+  });
+document.
+  getElementById('anterior')
+  .addEventListener("click", function() {
+    slide_anterior();
+  });
+
+function nova_posicao() {
+  for (let slide of slides) {
+    slide.classList.remove('imagens_visivel');
+    slide.classList.add('imagens_oculta');
+  }
+
+  slides[posicao].classList.add('imagens_visivel');
+}
+
+function proximo_slide() {
+  if (posicao === totalSlides - 1) {
+    posicao = 0;
+  } else {
+    posicao++;
+  }
+
+  nova_posicao();
+}
+
+function slide_anterior() {
+  if (posicao === 0) {
+    posicao = totalSlides - 1;
+  } else {
+    posicao--;
+  }
+
+  nova_posicao();
+}
